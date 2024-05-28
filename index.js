0658267a3b004cd9113bb5c5701194ce3e7fb110
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-28 11:14:16 -0400

    Render basic card component for all crypto

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 import { cryptoData } from "./api_data.js"
 
-console.log(cryptoData)
+const allCryptoList = document.querySelector('#all-crypto-list')
 
-const fetchCryptoData = () => {
+const getCryptoDataFromAPI = () => {
   fetch('https://api.coincap.io/v2/assets')
     .then(res => res.json())
-    .then(handleSuccess)
+    .then(handleFetchSuccess)
     .catch(handleError)
 }
 
-const handleSuccess = (data) => {
-  console.log(data)
+const handleFetchSuccess = (cryptoObject) => {
+  console.log(cryptoObject.data)
+  cryptoObject.data.forEach(crypto => {
+    let changeOverLast24
+    if(crypto.changePercent24Hr.slice(0,1) === '-'){
+      changeOverLast24 = `${crypto.changePercent24Hr.slice(0,5)}`
+    }else{
+      changeOverLast24 = `+${crypto.changePercent24Hr.slice(0,4)}`
+    } 
+    const cardDiv = document.createElement('div')
+    cardDiv.classList.add('crypto-card', 'mb-3')
+    cardDiv.innerHTML = `
+            <div class="icon">
+                <img width="50px" src="./64/${crypto.id}.png" />
+            </div>
+            <div class="details">
+                <div class="title">${crypto.name}</div>
+                <div class="info">
+                    <div class="icon-text">
+                        <i class="bi bi-clock"></i> 24h Change: ${changeOverLast24}%
+                    </div>
+                    <div class="icon-text">
+                        <i class="bi bi-geo-alt"></i> Current Price: $40,000
+                    </div>
+                </div>
+    `
+    allCryptoList.appendChild(cardDiv)
+  })
 }
 
 const handleError = (error) => {
@@ -18,4 +44,8 @@ const handleError = (error) => {
   alert('Something wen wrong during fetch!')
 }
 
-// fetchCryptoData()
+const initApp = () => {
+  handleFetchSuccess(cryptoData)
+}
+
+initApp()
