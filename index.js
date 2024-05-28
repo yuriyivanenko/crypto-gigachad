da7b5f5dccb36a55a650b4e24a6dfc67a9cd7042
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-28 13:31:45 -0400

    Refactor crypto card component
    Styling for each card is at an acceptable point.
    Added buttons to transact and research individual
    cryptos. No functionality added to buttons yet.

--- index.js
+++ index.js
@@ -9,34 +9,48 @@ const getCryptoDataFromAPI = () => {
     .catch(handleError)
 }
 
+const priceFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+const last24HrFormatter = (crypto) => {
+  if(crypto.changePercent24Hr.slice(0,1) === '-'){
+    return `${crypto.changePercent24Hr.slice(0,5)}`
+  }else{
+    return`+${crypto.changePercent24Hr.slice(0,4)}`
+  }
+}
+
 const handleFetchSuccess = (cryptoObject) => {
-  console.log(cryptoObject.data)
-  cryptoObject.data.forEach(crypto => {
-    let changeOverLast24
-    if(crypto.changePercent24Hr.slice(0,1) === '-'){
-      changeOverLast24 = `${crypto.changePercent24Hr.slice(0,5)}`
-    }else{
-      changeOverLast24 = `+${crypto.changePercent24Hr.slice(0,4)}`
-    } 
-    const cardDiv = document.createElement('div')
-    cardDiv.classList.add('crypto-card', 'mb-3')
-    cardDiv.innerHTML = `
-            <div class="icon">
-                <img width="50px" src="./64/${crypto.id}.png" />
-            </div>
-            <div class="details">
-                <div class="title">${crypto.name}</div>
-                <div class="info">
-                    <div class="icon-text">
-                        <i class="bi bi-clock"></i> 24h Change: ${changeOverLast24}%
-                    </div>
-                    <div class="icon-text">
-                        <i class="bi bi-geo-alt"></i> Current Price: $40,000
-                    </div>
-                </div>
-    `
-    allCryptoList.appendChild(cardDiv)
-  })
+  cryptoObject.data.forEach(renderCryptoCard)
+}
+
+const renderCryptoCard = (crypto) => {  
+  const cardDiv = document.createElement('div')
+  cardDiv.classList.add('crypto-card', 'mb-3')
+  cardDiv.innerHTML = `
+  <div class="icon">
+        <img width="50px" alt="${crypto.id}" src="./64/${crypto.id}.png" />
+    </div>
+    <div class="details">
+      <div class="title">${crypto.name}</div>
+      <div class="info">
+          <div class="icon-text"><i class="bi bi-clock px-2"></i> 24h Change: ${last24HrFormatter(crypto)}%</div>
+    </div>
+    </div>
+    <div class="d-flex w-100 justify-content-between align-items-center px-3 py-2">
+    <div class="price-container px-3">
+        <div class="icon-text">Price: ${priceFormatter.format(crypto.priceUsd)}</div>
+    </div>
+    <div class="px-3">
+        <button type="button" class="btn btn-secondary text-nowrap"><i class="bi bi-graph-down"></i> Research</button>
+    </div>
+    <div class="px-3">
+        <button type="button" class="btn btn-primary text-nowrap"><i class="bi bi-currency-bitcoin"></i> Buy / Sell</button>
+    </div>
+  </div>`
+  allCryptoList.appendChild(cardDiv)
 }
 
 const handleError = (error) => {
